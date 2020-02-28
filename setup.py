355c57bcd4a187f8ed354b1c296d744994491e6d
Joe Coburn <joe@scholarpack.com>
2020-02-28 13:24:43 +0000

    Fixed bug in packages stopping module import

--- setup.py
+++ setup.py
@@ -5,19 +5,20 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cookie-manager",
-    version="1.0.1",
+    version="1.0.2",
     author="ScholarPack",
     author_email="dev@scholarpack.com",
     description="Signed cookie manager for communication between multiple trusted services.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ScholarPack/cookie-manager",
-    packages=setuptools.find_packages(),
+    packages=["cookie_manager"],
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    install_requires=["itsdangerous >= 1.1.0"],
 )
